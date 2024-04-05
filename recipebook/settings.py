cc44e1bd15f24e8b12a684a19e7342af07132232
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2024-04-05 16:30:46 +0800

    Edited lines in settings

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR/'static'
+    BASE_DIR/'static',
 ]
+
+STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 
